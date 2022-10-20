06c9c548fea7de04050a6561d973d7d2c681676f
jeprianto24 <115442691+jeprianto24@users.noreply.github.com>
2022-10-20 23:08:49 +0800

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,39 @@
+nama = 'Jeprianto'
+usia = 19
+sudah_menikah = True
+
+print('nama:', nama)
+print('ujia:', usia)
+print('sudah menikah:', sudah_menikah)
+
+a, b, c = 1, 2, "Mantap"
+
+print('a:', a)
+print('b:', b)
+print('c:', c)
+
+# kita juga bisa memberikan satu nilai yang sama untuk
+# beberapa variabel
+
+d = e = f = 10
+
+print('d:', d)
+print('e:', e)
+print('f:', f)
+
+a = 'Madura'
+b = 50
+
+print(type(a))
+print(type(b))
+
+panjang = 5
+lebar = 10.5
+luas = panjang * lebar
+
+print(panjang, '*', lebar, '=', luas)
+print("Tipe dari variabel panjang:", type(panjang))
+print("Tipe dari variabel lebar:", type(lebar))
+print("Tipe dari variabel luas:", type(luas))
+
+
